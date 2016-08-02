5461b7c13e248227de3b3ef5b70e96ad2b9f5a8e
AustinDev16 <austin.blaser@gmail.com>
2016-08-02 10:14:40 -0600

    Added navigation between views, save, and cancel

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -19,11 +19,21 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     
     @IBOutlet weak var ratingControl: RatingControl!
     
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    
+    /* 
+    This value is passed by 'MealTableViewController' from the segue or as part of a new meal
+ */
+    var meal: Meal?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
         // Handle user input in text field through a delegate
         nameTextField.delegate = self
+        checkValidMealName()
+        
     }
 
     // MARK: UITextFieldDelegate
@@ -33,8 +43,19 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         return true
     }
     
+    func textFieldDidBeginEditing(textField: UITextField) {
+        // disable save button while typing
+        saveButton.enabled = false
+    }
+    
+    func checkValidMealName(){
+        let text = nameTextField.text ?? ""
+        saveButton.enabled = !text.isEmpty
+    }
+    
     func textFieldDidEndEditing(textField: UITextField) {
-        
+        checkValidMealName()
+        navigationItem.title = textField.text
     }
     
     // MARK: UIImagePickerControllerDelegate
@@ -51,6 +72,22 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         
         dismissViewControllerAnimated(true, completion: nil)
     }
+    // MARK: Navigation
+    
+    @IBAction func cancel(sender: UIBarButtonItem) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    // configure view controller before it's presented
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if saveButton === sender {
+            let name = nameTextField.text ?? ""
+            let photo = photoImageView.image
+            let rating = ratingControl.rating
+            
+            meal = Meal(name: name, photo: photo, rating: rating)
+        }
+    }
+    
     
     // MARK: Actions
     
